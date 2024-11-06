01ec7dac84c9ca2089bdfc909619d684b5731e08
francimar Barbosa Costa <mazinhobc3@hotmail.com>
2024-11-06 17:55:45 -0300

    cardapio

---
+++ cardapio.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cardapio PAZ Cofee</title>
+    <link rel="stylesheet" type="text/css" href="./css/stylecardapio.css">
+</head>
+<body>
+
+<div align = "center"><img class="img"  src="./img/1.png" alt=""></div>
+</body>
+</html>
