12670d70954f39a48b3551906fc1eeee1b613818
Francimar Barbosa Costa <mazinhobc3@hotmail.com>
2024-11-11 20:44:24 -0300

    auterações de botões

--- index.php
+++ index.php
@@ -1,71 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./css/inicial.css">
+    <link rel="shortcut icon" href="/img/logo_paz.ico" type="image/x-icon">
     <title>PAZ-STM</title>
 <style>
-body {
-    background-color: rgb(4, 4, 150);
-    background-image: url(../my-report/img/gradient_2.jpg);
-    background-repeat: no-repeat;
-    background-size: cover;
-    justify-content: center;
-    padding: 2px;
-    min-height: 90vh;
-    margin: 0;
-    color: aliceblue;    
-}
-
-img {
-    
-    text-align: center;
-    align-items: center;
-    color: aliceblue;
-    margin-top: 5px;
-    margin-bottom: 0;
-    padding-bottom: 0;
-    height: 20%;
-    width: 20%;
-    align-content: center;
-            }
-  
-            link {
-    text-align: center;
-    color: white;
-}            
-a {
-
-    display: inline-block;
-background-color: #4CAF50;
-color: #fff;
-border: none;
-padding: 10px 20px;
-font-size: 16px;
-font-weight: bold;
-text-decoration: none;
-cursor: pointer;
-border-radius: 5px;
-width: 200px; /* largura do botão */
-
-}
 
 </style>
 
 
 </head>
 <body>
-    <div class="img" align="center">
-    <img src="./img/paz.png" alt="">
+    <div>           
+    <img src="./img/logosup.png" class="logopaz"  alt="">
     </div>
 
-    <div align="center">
+<div>
  <ul class="link">
-<a class="link" href="index2.php">Enviar Relatório</a><br><br>
-<a href="https://whatsapp.com/channel/0029Vajb4YBGJP8ERCaAcS0h"><img src="./img/logo.png" alt="Descrição da imagem">Canal Whatsapp</a><br>
-<a href="https://www.youtube.com/@pazsantarempa">YouTube</a><br>
-<a href="https://www.instagram.com/pazsantarempa/">Instagram</a><br>
-<a href="https://www.facebook.com/PazSantaremPA">Facebook</a><br>
+<a href="index2.php" class="botao"><img src="./img/logo.png" class="imgbt" alt="Descrição da imagem">Relatório</a><br>
+<a href="https://whatsapp.com/channel/0029Vajb4YBGJP8ERCaAcS0h" class="botao"><img src="./img/whats.png" class="imgbt" alt="Descrição da imagem">Whatsapp</a><br>
+<a href="https://www.youtube.com/@pazsantarempa" class="botao"><img src="./img/youtube.png" class="imgbt" alt="Descrição da imagem">YouTube</a><br>
+<a href="https://www.instagram.com/pazsantarempa/" class="botao"><img src="./img/instagram.png" class="imgbt" alt="Descrição da imagem">Instagram</a><br>
+<a href="https://www.facebook.com/PazSantaremPA" class="botao"><img src="./img/face.png" class="imgbt" alt="Descrição da imagem">Facebook</a><br>
 </ul>
     </div>
     
