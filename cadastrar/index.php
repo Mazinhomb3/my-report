a130406cbacfb95eb1f777956dacba6742740e4c
Francimar Barbosa Costa <mazinhobc3@hotmail.com>
2024-11-17 12:47:35 -0300

    implentando cadastro

---
+++ cadastrar/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/cadastro.css">
+    
+    <link rel="shortcut icon" href="../img/logo_paz.ico" type="image/x-icon">
+    <title>Document</title>
+</head>
+<body>
+<div class="logo">           
+    <img src="../img/logosup.png" class="logopaz"  alt="">
+    </div>
+
+<div class="sub">
+    <h4>Cadastrar Surpervisores</h4>
+</div>
+    <div class="form">
+        <form action="login.php">
+            <table align="center" border="0" class="tabela">
+                <tr>
+                    <td class="inputs">Nome: </td>
+                    <td><input type="text" class="respostas"></td>
+                </tr>
+                <tr>
+                    <td>Senha: </td>
+                    <td><input type="password" class="respostas"></td>
+                </tr>
+            </table>
+        </form>
+    </div>
+</body>
+</html>
