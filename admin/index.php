c75df79a91de9fd0fd9e99f2ac4c2a85809ff1f8
francimar Barbosa Costa <mazinhobc3@hotmail.com>
2024-11-17 10:28:08 -0300

    implementar admin

--- admin/index.php
+++ admin/index.php
@@ -6,7 +6,49 @@ $corQuery="SELECT DISTINCT cor_rede FROM tbl_redes ORDER BY cor_rede ASC";
 $result = mysqli_query($conexao, $corQuery);
 
 
+$usuario = $_POST["senha"];
+$senha = $_POST["senha"];
+$cor_rede = $_POST["cor_rede"];
+$funcao = $_POST["funcao"];
+
+
+$sql = "INSERT INTO `tbl_login_sup` (`id`, `nome`, `rede`, `senha`, `funcao`) VALUES (NULL, 'Mazinho', 'ROYAL', MD5('43965148249'), 'admin');";
+
+$result = $conexao->query($sql);
+
+if ($result->num_rows > 0) {
+
+    $row = $result->fetch_assoc();
+
+    
+
+    $_SESSION['cod_lider_rede'] = $row['cod_lider_rede'];
+    $_SESSION['nome_lider'] = $row['nome_lider'];
+    $_SESSION['supervisor_rede_lider'] = $row['supervisor_rede_lider'];
+    $_SESSION['rede_lider'] = $row['rede_lider'];
+    $_SESSION['cor_rede_lider'] = $row['cor_rede_lider'];
+    $_SESSION['distrito_lider'] = $row['distrito_lider'];    
+    $_SESSION['area_lider'] = $row['area_lider'];
+    $_SESSION['setor_lider'] = $row['setor_lider'];
+    $_SESSION['data_lider'] = $row['data_lider'];
+
+    $_SESSION['mtp'] = $mtp;
+    $_SESSION['mcp'] = $mcp;
+    $_SESSION['cp'] = $cp;
+    $_SESSION['cria'] = $cria;
+    $_SESSION['tipo'] = $tipo;
+    $_SESSION['valor'] = $valor;
+ 
+
+    
+    header('Location: cadastro.php');
+  } else {
+    echo "Usuário ou senha incorretos.";
+  }
+
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -27,7 +69,7 @@ $result = mysqli_query($conexao, $corQuery);
     <h4>Aqui você terá acesso as dados de sua Rede.</h4>
     </div>
 <div class="form">
-    <form id="form" name="cadastrar" method="POST" action=""  onsubmit="return validaCampo(); return false;">
+    <form id="form" name="cadastrar" method="POST" action="" >
 
 <table align="center" border="1" class="table">
 <tr>
@@ -49,7 +91,7 @@ $result = mysqli_query($conexao, $corQuery);
     </tr>
     <tr>
         <td class="inputs">Senha: </td><td>
-        <input type="password" class="respostas"></tr>
+        <input name="senha" id="senha" type="password" class="respostas"></tr>
     </tr>
     
     <tr>
