98f39ede1710f87da88cb40cdd8bace079976f40
Mazinhomb3 <mazinhobc3@hotmail.com>
2024-11-29 21:49:54 -0300

    looker

---
+++ lider.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div align="center">
+    <iframe width="1200" height="600" src="https://lookerstudio.google.com/embed/reporting/46214fd1-d818-4ae4-bc78-1a43048cd40f/page/d6bVE" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+        </div>
+    </body>
+</html>
