107c0b78c6c72e064e7e9bbf6e90e14babec50c2
francimar Barbosa Costa <mazinhobc3@hotmail.com>
2024-12-16 15:44:19 -0300

    cor e setor auterado

--- admin/cor.php
+++ admin/cor.php
@@ -1,113 +1,25 @@
 <?php
-/*
-if (!isset($_SESSION))
 
-    session_start();
+require '../conexao.php';
 
-$nivel_necessario = 1;
+$sql = "SELECT lider_cel_rede FROM tbl_redes WHERE cor_rede='verde laguna' AND area_rede='bruno' AND setor_rede='bruno'";
 
-// Verifica se não há a variável da sessão que identifica o usuário
-if (!isset($_SESSION["nome"]) or ($_SESSION["nivel"] < $nivel_necessario)) {
-    // Destrói a sessão por segurança
-    session_destroy();
-    // Redireciona o visitante de volta pro login
-    header("Location: index.php");
-    exit;
-}
+$result = $conexao->query($sql);
 
-if (!empty($_POST["setorlider"])) {
-    $nome = $_SESSION['nome'];
-    $dtini = $_SESSION['dtini'];
-    $arealider = $_SESSION['arealider'];
-    $correde = $_SESSION['correde'];
-    $_SESSION['setorlider'] = $setorlider = $_POST['setorlider'];
-} else {
-    $nome = $_SESSION['nome'];
-    $dtini = $_SESSION['dtini'];
-    $arealider = $_SESSION['arealider'];
-    $correde = $_SESSION['correde'];
-    $setorlider = $_SESSION['setorlider'];
-    //unset($_SESSION['setorlider']);
-}
-*/
-require('../conexao.php');
-
-//header("refresh: 60; url=https://my-report.site/admin");
-
-$corQuery = "SELECT nome_lider, cod_lider_rede, id_rede FROM `tbl_dados` WHERE cor_rede_lider='verde laguna' AND area_lider='bruno'AND setor_lider='bruno' AND data_lider>='2024-12-02'";
-
-$result = mysqli_query($conexao, $corQuery);
-
-while ($row = mysqli_fetch_assoc($result)) {
-    $nome1 = $row['nome_lider'];
-    print_r($nome1);
-}
-
-?>
-
-
-
-<!DOCTYPE html>
-<html lang="pt-BR">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../css/admin.css">
-    <link rel="shortcut icon" href="../img/logo_paz.ico" type="image/x-icon">
-    <title>Paz Santarém</title>
-</head>
-
-<body>
-
-    <div class="sessao">
-        <?php echo "Bem-vindo, " . $_SESSION['nome'] ?><br>
-        <h3>Lider de Célula da Rede <?php echo $correde; ?>.</h3>
-
-    </div>
-    <div>
-
-        <table border="0" align="center">
-
-            <?php
-            $sql = "SELECT `lider_cel_rede`, cod_lider_rede FROM `tbl_redes` WHERE cor_rede like 'verde laguna' and `area_rede` like 'bruno' and setor_rede like 'bruno' ORDER BY lider_cel_rede ASC ";
-
-            $resultlider = mysqli_query($conexao, $sql);
-
-            while ($rowlider = mysqli_fetch_assoc($resultlider)) {
-
-                $nome2=array($rowlider["lider_cel_rede"]);
-              
-
-            ?>
-
-                <td class="<?php echo $cor ?>"><?php echo $rowlider["lider_cel_rede"] ?><br>
-                </td>
-                </tr>
-            <?php   } ?>
-        </table>
-        <tr>
-            <td><input class="botaoadmin" type="button" value="Detalhes" onClick="JavaScript: location.replace('detalhes.php');"></td><br>
-        </tr>
-        <tr>
-            <td><input class="botaoadmin" type="button" value="Voltar" onClick="JavaScript: location.replace('area.php');"></td>
-        </tr>
-
-    </div>
-</body>
-
-</html>
-
-<?php
-foreach ($result as $n) {
-    foreach ($resultlider as $n2) {
-        if ($n == $n2) {
-            $nome3[] = $n2;
-        }
+echo "<table border='1'>";
+while ($row = $result->fetch_assoc()) {
+    $sql_verificar = "SELECT id_rede FROM tbl_dados WHERE nome_lider = '" . $row["lider_cel_rede"] . "' AND data_Lider>='2024-12-02'";
+    $result_verificar = $conexao->query($sql_verificar);
+    $quantidade = mysqli_fetch_assoc($result_verificar);
+    
+    if ($quantidade > 1) {
+        echo "<tr style='background-color: #FFC080;'>"; // Cor de fundo para repetidas
+    } else {
+        echo "<tr>";
     }
-} 
-//var_dump($n);
-//var_dump($n2);
-//var_dump($result);
+    
+    echo "<td>" . $row["lider_cel_rede"] . "</td></tr>";
+}
+echo "</table>";
 
 ?>
--- admin/setor3.php
+++ admin/setor3.php
@@ -34,8 +34,6 @@ require('../conexao.php');
 
 //header("refresh: 60; url=https://my-report.site/admin");
 
-$cor = "";
-
 
 ?>
 
@@ -65,36 +63,48 @@ $cor = "";
         <h3>Celulas setor do <?php echo $_SESSION['setorlider'] ?></h3>
 
         <table border="0" align="center" id="segunda">
+            <?php
+            
+            
+            ?>
+        
+        <tr>
+               
+            </tr>
+        
+        
+        </table>
+
+        <h3>Relatórios enviados.</h3>
+        <table border="0" align="center" id="primeira">
             <tr>
+
                 <?php
-                $corQuery = "SELECT `lider_cel_rede`, cod_lider_rede FROM `tbl_redes` WHERE cor_rede like '$correde' and `area_rede` like '$arealider' 
-                and setor_rede like '$setorlider' ORDER BY lider_cel_rede ASC ";
+                $corQuery = "SELECT DISTINCT nome_lider, cod_lider_rede FROM `tbl_dados` WHERE cor_rede_lider like '$correde' and `area_lider` like '$arealider' 
+                and setor_lider like '$setorlider' and data_lider >= '$dtini' order by nome_lider asc ";
 
                 $result = mysqli_query($conexao, $corQuery);
 
-                while ($row1 = mysqli_fetch_assoc($result)) {
-
-                    $sql_verificar = "SELECT id_rede FROM tbl_dados WHERE nome_lider = '" . $row1["lider_cel_rede"] . "' AND data_Lider>='2024-12-02'";
-                    $result_verificar = $conexao->query($sql_verificar);
-                    $quantidade = mysqli_fetch_assoc($result_verificar);
-
-                    if ($quantidade > 1) {
-                        $cor = "respostasverd"; // Cor de fundo para repetidas
-                    } else {
-                        $cor =  "respostas";
+                while ($row2 = mysqli_fetch_assoc($result)) { 
+                    $id2 = array($row2['cod_lider_rede']);
+                    
+                    foreach ($id2 as $n) {
+                        foreach ($id1 as $n2) {
+                            if ($n == $n2) {
+                                 echo"verde";
+                            }
+                        }
                     }
+                    ?>
+                    <td>
 
-                ?>
-
-                    <td class="<?php echo $cor ?>"><?php echo $row1["lider_cel_rede"] ?><br>
-                       
+                    <td class="<?php echo $cor ?>"><?php echo $row2['nome_lider']; ?><br>
                     </td>
-
             </tr>
+
         <?php   } ?>
-
+                   
         </table>
-
 
         <tr>
             <td><input class="botaoadmin" type="button" value="Detalhes" onClick="JavaScript: location.replace('detalhes.php');"></td><br>
@@ -107,3 +117,4 @@ $cor = "";
 </body>
 
 </html>
+
